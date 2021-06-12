e34e7c45b933e99704321e16c59b059046c00c80
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-06-12 06:33:16 +0200

    Removed wrong comment

--- 74/bday.py
+++ 74/bday.py
@@ -3,5 +3,4 @@ import calendar
 
 def weekday_of_birth_date(date):
     """Takes a date object and returns the corresponding weekday string"""
-    # Split it to year, month and day
     return calendar.day_name[date.weekday()]
