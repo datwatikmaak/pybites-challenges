f11ae43e02c62395d26b70fa89cc56fb31db86cb
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-06-15 05:47:12 +0200

    Function get_all_jeeps() and get_first_model_each_manufacturer() are working

--- 21/cars.py
+++ 21/cars.py
@@ -10,22 +10,42 @@ cars = {
 def get_all_jeeps(cars=cars):
     """return a comma  + space (', ') separated string of jeep models
        (original order)"""
-    pass
+    string_of_jeeps = ", "
+    jeep_models = cars['Jeep']
+    return string_of_jeeps.join(jeep_models)
+
+
+get_all_jeeps(cars)
 
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
-    pass
+    first_models = []
+    for key, value in cars.items():
+        first_models.append(value[0])
+
+    return first_models
+
+
+get_first_model_each_manufacturer(cars)
 
 
 def get_all_matching_models(cars=cars, grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    pass
+    if " " in cars.values():
+        return cars.values()
+
+
+get_all_matching_models(cars)
 
 
 def sort_car_models(cars=cars):
     """return a copy of the cars dict with the car models (values)
        sorted alphabetically"""
-    pass
+    copied_dict = cars.copy()
+    return sorted(copied_dict.values(), reverse=True)
+
+
+sort_car_models(cars)
