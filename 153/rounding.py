964db7c98b3cc0865e3857d3787cddf19851d810
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-06-15 16:37:19 +0200

    Started working on the challenge

--- 153/rounding.py
+++ 153/rounding.py
@@ -1,9 +1,12 @@
+import math
+
+
 def round_up_or_down(transactions, up=True):
     """Round the list of transactions passed in.
        If up=True (default) round up, else round down.
        Return a new list of rounded values
     """
-    return [round(num) for num in transactions]
+    print(list(round(num) for num in transactions))
 
 
-round_up_or_down(transactions=list)
+round_up_or_down()
