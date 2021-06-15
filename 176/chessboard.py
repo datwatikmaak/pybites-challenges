3d6c452ac877f8fe8896045ed8f28bcf8c932c2f
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-06-15 17:20:08 +0200

    Started to solve the challenge

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -4,4 +4,9 @@ WHITE, BLACK = ' ', '#'
 def create_chessboard(size=8):
     """Create a chessboard with of the size passed in.
        Don't return anything, print the output to stdout"""
-    pass
+    r = range(size)
+    rows = [' '.join(['01'[(i + p) % 2] for i in r]) for p in (WHITE, BLACK)]
+    return [rows[i % 2] for i in r]
+
+
+create_chessboard(8)
