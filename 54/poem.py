de75096d231c797154e3fce4ce0678c9d25d55fa
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-06-16 10:43:41 +0200

    Removed the spaces at the beginning of the lines

--- 54/poem.py
+++ 54/poem.py
@@ -1,5 +1,20 @@
 INDENTS = 4
 
+shakespeare_unformatted = """
+                          To be, or not to be, that is the question:
+                          Whether 'tis nobler in the mind to suffer
+
+                          The slings and arrows of outrageous fortune,
+                          Or to take Arms against a Sea of troubles,
+                          """
+
 
 def print_hanging_indents(poem):
-    pass
+
+    result = "\n".join([x.strip(" ") for x in "".join(poem).split("\n")])
+
+    print(result)
+    print(repr(result))
+
+
+print_hanging_indents(shakespeare_unformatted)
