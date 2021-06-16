b63ad432ae8ba497096060dd02cefb85dd50ae94
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-06-16 05:56:49 +0200

    submission Bite 176 @ codechalleng.es

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -4,15 +4,13 @@ WHITE, BLACK = ' ', '#'
 def create_chessboard(size=8):
     """Create a chessboard with of the size passed in.
        Don't return anything, print the output to stdout"""
-    # outer loop
     for i in range(size):
-        # inner loop
         for j in range(size):
-            if j % 2 == 0:
-                print(WHITE, end=" ")
+            if (i + j) % 2 == 0:
+                print(WHITE, end='')
             else:
-                print(BLACK, end=" ")
-        print(" ")
+                print(BLACK, end='')
+        print()
 
 
-create_chessboard(size)
+create_chessboard(8)
