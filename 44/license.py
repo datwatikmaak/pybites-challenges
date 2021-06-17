ed1b785a5ed010e80e4116e357a6277fa1283ae3
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-06-17 09:50:47 +0200

    submission Bite 44 @ codechalleng.es

--- 44/license.py
+++ 44/license.py
@@ -1,19 +1,18 @@
 import string
-import random
+import secrets
 
 
 def gen_key(parts=4, chars_per_part=8):
     chars = string.ascii_uppercase + string.digits
-    # token = ''.join(random.choice(chars) for i in range(chars_per_part))
 
     license_key = ""
     for i in range(parts):
         if i < parts - 1:
-            license_key += f"{''.join(random.choice(chars) for i in range(chars_per_part))}-"
+            license_key += f"{''.join(secrets.choice(chars) for i in range(chars_per_part))}-"
         else:
-            license_key += f"{''.join(random.choice(chars) for i in range(chars_per_part))}"
+            license_key += f"{''.join(secrets.choice(chars) for i in range(chars_per_part))}"
 
-    print(license_key)
+    return license_key
 
 
 gen_key()
