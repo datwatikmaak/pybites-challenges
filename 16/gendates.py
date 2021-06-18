60a3f76f0eaeebccac03ad1dd53ca68cabb197eb
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-06-18 11:06:00 +0200

    submission Bite 16 @ codechalleng.es

--- 16/gendates.py
+++ 16/gendates.py
@@ -1,7 +1,16 @@
+import itertools
 from datetime import datetime, timedelta
 
 PYBITES_BORN = datetime(year=2016, month=12, day=19)
+START_DATE = PYBITES_BORN + timedelta(days=100)
 
 
 def gen_special_pybites_dates():
-    pass
+    from_date = START_DATE
+    while True:
+        yield from_date
+        from_date = from_date + timedelta(days=100)
+
+
+dates = itertools.islice(gen_special_pybites_dates(), 10)
+list(dates)
