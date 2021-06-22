c84060b87f5681413d14ccf594ca463219e3a2df
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-06-22 11:14:26 +0200

    submission Bite 161 @ codechalleng.es

--- 161/tree.py
+++ 161/tree.py
@@ -5,15 +5,14 @@ def count_dirs_and_files(directory='.'):
     """Count the amount of of directories and files in passed in "directory" arg.
        Return a tuple of (number_of_directories, number_of_files)
     """
-    total_dirs = 0
-    total_files = 0
+    number_of_directories = 0
+    number_of_files = 0
 
     for root, dirs, files in os.walk(directory):
-        total_dirs += len(dirs)
-        total_files += len(files)
+        number_of_directories += len(dirs)
+        number_of_files += len(files)
 
-    print(f"Aantal dirs: {total_dirs}")
-    print(f"Aantal files: {total_files}")
+    return number_of_directories, number_of_files
 
 
 count_dirs_and_files()
