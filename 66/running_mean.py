ef2253a98adbdd3dddfc2eb3c21585c1325f0e48
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-06-29 05:24:56 +0200

    submission Bite 66 @ codechalleng.es

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -1,5 +1,15 @@
+import itertools
+
+
 def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
        returns a sequence of same length with the averages.
        You can assume all items in sequence are numeric."""
-    pass
+
+    sums = itertools.accumulate(sequence)
+    averages = [accum / i for i, accum in enumerate(sums, start=1)]
+
+    return [round(elem, 2) for elem in averages]
+
+
+running_mean(sequence=[1, 2, 3])
